fe7cb1a1b1f5995e1f9b50c84027a7d1b4d4926f
Brian Bycynski <high5bri@hotmail.com>
2018-02-02 07:45:02 -0500

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import SimpleCounter from './Simple_Counter'
+import VisibilityToggle from './VisibilityToggle'
 
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    return (
+      <div className='app'>
+        <SimpleCounter />
+        <hr />
+        <VisibilityToggle />
+      </div>
+    )
   }
 }
 
